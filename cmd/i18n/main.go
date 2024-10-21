6a4a97f574d0bac331744fbb4c6d6559deea1892
MrNemo64 <58314381+MrNemo64@users.noreply.github.com>
2024-10-21 20:45:19 +0200

    change usage message

--- cmd/i18n/main.go
+++ cmd/i18n/main.go
@@ -2,7 +2,6 @@ package main
 
 import (
 	"flag"
-	"fmt"
 	"log/slog"
 	"os"
 
@@ -12,22 +11,20 @@ import (
 func main() {
 	defaultLanguage := flag.String("default-language", "", "Specifies the default language")
 	messagesDir := flag.String("messages", "", "Specifies the directory with the files with the messages")
-	var outFile, outPackage string
-	flag.StringVar(&outFile, "out-file", "generated_lang.go", "Specifies the output file with the messages")
-	flag.StringVar(&outPackage, "out-package", os.Getenv("GOPACKAGE"), "Specifies the output file with the messages")
-
+	outFile := flag.String("out-file", "generated_lang.go", "Specifies the output file with the messages")
+	outPackage := flag.String("out-package", os.Getenv("GOPACKAGE"), "Specifies the output file with the messages")
 	flag.Parse()
 
 	if *defaultLanguage == "" || *messagesDir == "" {
-		fmt.Println("Usage: code-generator -default-language <lang> -messages <dir> -out-file <output file> -out-package <output-package>")
+		flag.Usage()
 		os.Exit(1)
 	}
 
 	cli.Main(cli.CliArgs{
 		MessagesDirectory: *messagesDir,
 		DefaultLanguage:   *defaultLanguage,
-		OutFile:           outFile,
-		Package:           outPackage,
+		OutFile:           *outFile,
+		Package:           *outPackage,
 		LogLevel:          slog.LevelDebug,
 	})
 }
