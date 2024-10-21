223036a6b4825169ecc32d1a798c6f8e8a0c89d5
MrNemo64 <58314381+MrNemo64@users.noreply.github.com>
2024-10-21 19:52:40 +0200

    add more program arguments

--- cmd/i18n/main.go
+++ cmd/i18n/main.go
@@ -12,19 +12,22 @@ import (
 func main() {
 	defaultLanguage := flag.String("default-language", "", "Specifies the default language")
 	messagesDir := flag.String("messages", "", "Specifies the directory with the files with the messages")
+	var outFile, outPackage string
+	flag.StringVar(&outFile, "out-file", "generated_lang.go", "Specifies the output file with the messages")
+	flag.StringVar(&outPackage, "out-package", os.Getenv("GOPACKAGE"), "Specifies the output file with the messages")
 
 	flag.Parse()
 
 	if *defaultLanguage == "" || *messagesDir == "" {
-		fmt.Println("Usage: code-generator -default-language <lang> -messages <dir>")
+		fmt.Println("Usage: code-generator -default-language <lang> -messages <dir> -out-file <output file> -out-package <output-package>")
 		os.Exit(1)
 	}
 
 	cli.Main(cli.CliArgs{
 		MessagesDirectory: *messagesDir,
 		DefaultLanguage:   *defaultLanguage,
-		OutFile:           "generated_lang.go",
-		Package:           os.Getenv("GOPACKAGE"),
+		OutFile:           outFile,
+		Package:           outPackage,
 		LogLevel:          slog.LevelDebug,
 	})
 }
